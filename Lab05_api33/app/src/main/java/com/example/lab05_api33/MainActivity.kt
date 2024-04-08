b39436e6657842eb443a599ca5e35409ce6ce4fd
matt2ology <34324060+matt2ology@users.noreply.github.com>
2024-04-08 11:40:18 -0700

    Added the App Title text on screen

--- Lab05_api33/app/src/main/java/com/example/lab05_api33/MainActivity.kt
+++ Lab05_api33/app/src/main/java/com/example/lab05_api33/MainActivity.kt
@@ -9,7 +9,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.lab05_api33.ui.theme.Lab05_api33Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,28 +22,33 @@ class MainActivity : ComponentActivity() {
             Lab05_api33Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), // Fill the container with the content
+                    color = MaterialTheme.colorScheme.background // Set the background color of the UI
                 ) {
-                    Greeting("Android")
+                    // Display the text "Android Pizza Shop" with specified text size and style
+                    TitleText("Android Pizza Shop")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+@Composable // Composable function for displaying text in the UI (Jetpack Compose)
+fun TitleText(text: String) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = text, // Text to be displayed in the UI
+        fontSize = 34.sp, // Text size in scale-independent pixels
+        fontWeight = FontWeight.Bold, // Text weight (bold)
+        textAlign = TextAlign.Center, // Text alignment (center)
+        modifier = Modifier.fillMaxSize() // Fill the container with the content
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+@Preview(showBackground = true) // Preview the UI with a background color (Jetpack Compose)
+@Composable // Composable function for previewing the UI in the IDE (Jetpack Compose)
+fun TitleTextPreview(){
     Lab05_api33Theme {
-        Greeting("Android")
+        // Preview using TitleText composable
+        TitleText("Android Pizza Shop")
     }
 }
